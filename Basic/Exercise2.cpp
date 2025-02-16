452b311a05699f96d18ca8eb7856129eeb2d91d2
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-02-16 07:30:23 -0500

    Create Exercise2.cpp

---
+++ Basic/Exercise2.cpp
@@ -0,0 +1,11 @@
+// Write a program in C++ to print the sum of two numbers.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Print the sum of two numbers: " << endl;
+    cout << "-----------------------------" << endl;
+    cout << "The sum of 29 and 30 is : " << 29+30 << endl;
+}
