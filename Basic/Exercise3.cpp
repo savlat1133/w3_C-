0ef3dd503c2c7b01a727803ada9b69722fa34ab7
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-02-16 07:37:52 -0500

    Create Exercise3.cpp

---
+++ Basic/Exercise3.cpp
@@ -0,0 +1,19 @@
+// Size of Fundamental Data Types
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Find Size of fundamental data types : " << endl;
+    cout << "---------------------------------" << endl;
+    cout << "The sizeof(char) is : "<< sizeof(char) << " bytes" << endl;
+    cout << "The sizeof(short) is : "<< sizeof(short) << " bytes"  << endl;
+    cout << "The sizeof(int) is : "<< sizeof(int) << " bytes"  << endl;
+    cout << "The sizeof(long) is : "<< sizeof(long) << " bytes"  << endl;
+    cout << "The sizeof(long long) is : "<< sizeof(long long) << " bytes"  << endl;
+    cout << "The sizeof(float) is : "<< sizeof(float) << " bytes"  << endl;
+    cout << "The sizeof(double) is : "<< sizeof(double) << " bytes"  << endl;
+    cout << "The sizeof(long double) is : "<< sizeof(long double) << " bytes"  << endl;
+    cout << "The sizeof(bool) is : "<< sizeof(bool) << " bytes"  << endl;
+}
