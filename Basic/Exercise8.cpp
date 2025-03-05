bd39e5f7786ba266cc2871ddcdc69c1fcd612be0
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-03-05 12:40:02 -0500

    Create Exercise8.cpp

---
+++ Basic/Exercise8.cpp
@@ -0,0 +1,23 @@
+// Write a C++ program to check overflow/underflow during various arithmetic operations.
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Check overflow/underflow during various arithmetical operation: " << endl;
+    cout << "Range of int is [-2147483648, 2147483647]" << endl;
+    cout << "---------------------------------------------------------------" << endl;
+    int n1 = 2147483647;
+    cout <<" Overflow the integer range and set in minimum range : " << n1 + 1 << endl;
+    cout <<" Increasing from its minimum range : " << n1 + 2 << endl;
+    cout <<" Product is :" << n1 * n1 << endl; 
+
+    int n2 = -2147483648;
+    cout <<" Underflow the range and set in maximum range : " << n2 - 1 << endl;
+    cout <<" Decreasing from its maximum range : " << n2 - 2 << endl;
+    cout <<" Product is : " << n2 * n2 << endl;
+
+    cout << endl;
+    return 0;
+
+}
