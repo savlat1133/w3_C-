c787e5333786875eabaf2025ac98e9268ffa280d
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-03-15 13:39:14 -0400

    Create Exercise12.cpp

---
+++ Basic/Exercise12.cpp
@@ -0,0 +1,19 @@
+ // Add Two Numbers from Keyboard
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Sum of two numbers : " << endl;
+    cout << "----------------------" << endl;
+
+    int num1, num2;
+    cout << "Input 1st number : ";
+    cin >> num1;
+    cout << "Input 2nd number : ";
+    cin >> num2;
+    cout << "Sum of two numbers : " << num1 + num2 << endl;
+
+    return 0;
+}
