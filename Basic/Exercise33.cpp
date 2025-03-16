2a821410bf0bb04c29b7d6666a416203a333cbc9
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-03-15 21:04:25 -0400

    Create Exercise33.cpp

---
+++ Basic/Exercise33.cpp
@@ -0,0 +1,23 @@
+// Divide Two Numbers and Print Quotient
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Divide two numbers and print:" << endl;
+    cout << "----------------------------------------------" << endl;
+
+    int number1, number2;
+    cout << "Input a first number: ";
+    cin >> number1;
+    cout << "Input an second number: ";
+    cin >> number2;
+
+    cout << "The quotient of " << number1 << " and " << number2 << " is : " << number1/number2 << endl;
+
+
+
+    return 0;
+}
