068121b405099a798ec714403077a6c1f5de3138
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-03-18 18:52:57 -0400

    Create Exercise39.cpp

---
+++ Basic/Exercise39.cpp
@@ -0,0 +1,21 @@
+// Print Custom ASCII Pattern
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Print the following pattern: " << endl;
+    cout << "----------------------------------------------" << endl;
+    cout << " xxxxx\n";
+    cout << "x     x       x        x\n";
+    cout << "x             x        x\n";
+    cout << "x          xxxxxxx  xxxxxxx\n";
+    cout << "x             x        x\n";
+    cout << "x     x       x        x\n";
+    cout << " xxxxx\n";
+
+
+    return 0;
+}
