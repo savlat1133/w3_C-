6c3f59546bc2d499da394d87b5c0152711d6e7e1
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-03-18 18:32:30 -0400

    Create Exercise35.cpp

---
+++ Basic/Exercise35.cpp
@@ -0,0 +1,16 @@
+// Compute Specified Expression
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Compute the specified expressions and print the output: " << endl;
+    cout << "----------------------------------------------" << endl;
+
+    cout << "Result of the expression (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is : " << (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) << endl;
+
+
+    return 0;
+}
