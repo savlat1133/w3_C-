cc3830776d56e0e2f6abb4e7a83510a8397605d6
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-03-18 18:55:43 -0400

    Create Exercise41.cpp

---
+++ Basic/Exercise41.cpp
@@ -0,0 +1,31 @@
+// Print American Flag Pattern
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Print the American Flag : " << endl;
+    cout << "----------------------------------------------" << endl;
+    cout << "* * * * * * ==================================\n"; // First row of the flag
+    cout << " * * * * *  ==================================\n"; // Second row of the flag
+    cout << "* * * * * * ==================================\n"; // Third row of the flag
+    cout << " * * * * *  ==================================\n"; // Fourth row of the flag
+    cout << "* * * * * * ==================================\n"; // Fifth row of the flag
+    cout << " * * * * *  ==================================\n"; // Sixth row of the flag
+    cout << "* * * * * * ==================================\n"; // Seventh row of the flag
+    cout << " * * * * *  ==================================\n"; // Eighth row of the flag
+    cout << "* * * * * * ==================================\n"; // Ninth row of the flag
+
+    // Outputting the lower part of the flag representing the stripes
+    cout << "==============================================\n"; // Rows representing the stripes of the flag (bottom)
+    cout << "==============================================\n";
+    cout << "==============================================\n";
+    cout << "==============================================\n";
+    cout << "==============================================\n";
+    cout << "==============================================\n";
+    cout << "==============================================\n";
+
+    return 0;
+}
