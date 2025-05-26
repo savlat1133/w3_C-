d632e5159e8f7a37fb0864a1f737af2f5b0e4ed5
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-05-26 16:10:39 -0400

    Create Exercise1.cpp

---
+++ For-Loops/Exercise1.cpp
@@ -0,0 +1,18 @@
+// 1. First 10 Natural Numbers
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "First 10 Natural Numbers" << endl;
+    cout << "-------------------------------" << endl;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    
+    return 0;
+}
