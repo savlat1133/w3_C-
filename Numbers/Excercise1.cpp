eae1de4684df5e459427239dbdceca690daf9ae7
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-07-06 20:19:31 -0400

    Create Excercise1.cpp

---
+++ Numbers/Excercise1.cpp
@@ -0,0 +1,29 @@
+// 1. Ugly Number Check
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool ugly_number(int num) {
+    if (num == 2) {
+        return true;
+    }
+    for (int i = num - 1; i >= 2; --i) {
+        if ((num % i == 0 && i != 5) && (num % i == 0 && i != 3) && (num % i == 0 && i != 2)){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+int main() {
+    cout << "Dynamically allocate two two-dimensional arrays of floating values and strings" << endl;
+    cout << "-------------------------------------------" << endl;
+
+    cout << ugly_number(15) << endl;
+
+
+
+    return 0;
+}
