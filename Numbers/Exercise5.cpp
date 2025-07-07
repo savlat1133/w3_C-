a693dcd5d92543f9bc1ecabfb299960eafa67f42
Savlatjon <77123400+savlat1133@users.noreply.github.com>
2025-07-06 20:53:11 -0400

    Create Exercise5.cpp

---
+++ Numbers/Exercise5.cpp
@@ -0,0 +1,41 @@
+// 5. Find Perfect Numbers and Count Between 1 and 1000
+
+#include <iostream>
+#include <string>
+using namespace std;
+bool abundant_number(int number) {
+    int sum = 0;
+
+    for (int i = number - 1; i >= 1; --i) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+    if (sum > number) {
+        return true;
+    }
+    return false;
+}
+
+
+
+
+int main() {
+    cout << "find abundant numbers between 1 and 1000" << endl;
+    cout << "-------------------------------------------" << endl;
+
+    int end = 50;
+    for (int i = 1; i <= 1000; ++i) {
+        if (abundant_number(i)) {
+            cout << i << " ";
+        }
+        if (i == end) {
+            cout << endl;
+            end += 50;
+        }
+    }
+
+
+
+    return 0;
+}
